0029ff483831696cc6b51e63929adb3304d9b536
giwiro <giwirodavalos@gmail.com>
2017-10-22 16:27:41 -0500

    editorconfig added because i handle lots of proyects with diff settings, and separate logic from container

--- src/containers/Cipher.jsx
+++ src/containers/Cipher.jsx
@@ -1,6 +1,7 @@
 // @flow
 import * as React from 'react';
 import CipherForm from '../component/CipherForm';
+import { encrypt } from '../lib/crypto';
 
 type State = {
   phrase: string,
@@ -8,47 +9,17 @@ type State = {
   ePhrase: string,
 }
 
-// Padding to take care
-const padding = 17;
-// a
-const baseCharCode = 97;
-// z
-const endCharCode = 122;
-
-const ex = (a: string, b: string) => {
-  let r = (((a.charCodeAt(0) + b.charCodeAt(0)) - 97) % 26) + 97;
-  r = r >= (baseCharCode + padding) ? r - padding : endCharCode - (padding - (r - baseCharCode));
-  // console.log(`${a} + ${b} = ${String.fromCharCode(r)}`);
-  return String.fromCharCode(r);
-};
-
 export default class Cipher extends React.Component<{}, State> {
   state = {
-    phrase: 'hola chau chau',
+    phrase: 'hola chau',
     keyword: 'bye',
     ePhrase: '',
   };
 
   handleSubmit(event: Event) {
     event.preventDefault();
-    this.encrypt(this.state.phrase, this.state.keyword);
-  }
-
-  encrypt(phrase: string, keyword: string) {
-    const phraseLen = phrase.length;
-    const keywordLen = keyword.length;
-    let r = '';
-    let hopCount = 0;
-    for (let i = 0; i < phraseLen; i ++) {
-      if (phrase[i] === ' ') {
-        r += ' ';
-        hopCount++;
-      } else r += ex(phrase[i], keyword[(i - hopCount) % keywordLen]);
-    }
-    this.setState({
-        ePhrase: r,
-      }
-    )
+    const ePhrase = encrypt(this.state.phrase, this.state.keyword);
+    this.setState({ ePhrase });
   }
 
   handleChange(event: SyntheticEvent<HTMLInputElement>) {
---
+++ src/lib/crypto.jsx
@@ -0,0 +1,29 @@
+// @flow
+
+// Padding to take care
+const padding = 17;
+// a
+const baseCharCode = 97;
+// z
+const endCharCode = 122;
+// alphabet size
+const alphabetSize = 26;
+
+const ex = (a: string, b: string) => {
+  let r = (((a.charCodeAt(0) + b.charCodeAt(0)) - baseCharCode) % alphabetSize) + baseCharCode;
+  r = r >= (baseCharCode + padding) ? r - padding : endCharCode - (padding - (r - baseCharCode));
+  // console.log(`${a} + ${b} = ${String.fromCharCode(r)}`);
+  return String.fromCharCode(r);
+};
+
+export function encrypt(phrase: string, keyword: string): string {
+  let r = '';
+  let hopCount = 0;
+  for (let i = 0; i < phrase.length; i ++) {
+    if (phrase[i] === ' ') {
+      r += ' ';
+      hopCount++;
+    } else r += ex(phrase[i], keyword[(i - hopCount) % keyword.length]);
+  }
+  return r;
+}
